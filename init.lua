109133e8ffa3761f0da72ae500fca831e3f1be2a
andreapisa9 <andrew9.ap@gmail.com>
2025-05-22 19:17:14 +0200

    Indent Guides

--- init.lua
+++ init.lua
@@ -972,6 +972,17 @@ require('lazy').setup({
     'm4xshen/autoclose.nvim',
     opts = {},
   },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      indent = {
+        char = '│',
+      },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.

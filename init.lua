da9116eb14f8b82993e56db4c3e9525d2abfe742
andreapisa9 <andrew9.ap@gmail.com>
2025-11-29 17:28:55 +0100

    feat(init): add Zig support

--- init.lua
+++ init.lua
@@ -1134,6 +1134,13 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'ziglang/zig.vim',
+    ft = 'zig',
+    config = function()
+      require('zig').setup()
+    end,
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.

e3f7361cfc471e08165b8bfec4b7df2a3fbbcaca
andreapisa9 <andrew9.ap@gmail.com>
2025-12-25 18:13:35 +0100

    feat(init): add harpoon

--- init.lua
+++ init.lua
@@ -465,6 +465,61 @@ require('lazy').setup({
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+
+    config = function()
+      local harpoon = require 'harpoon'
+
+      --REQUIRED
+      harpoon:setup {}
+      --REQUIRED
+
+      -- basic telescope configuration
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+
+      vim.keymap.set('n', '<C-e>', function()
+        toggle_telescope(harpoon:list())
+      end, { desc = 'Open harpoon window' })
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = 'Add to harpoon list' })
+      vim.keymap.set('n', 'C-1', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', 'C-2', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', 'C-3', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', 'C-4', function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
 
   -- LSP Plugins
   {
